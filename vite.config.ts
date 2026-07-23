e1432c48686d9ff061cc7182ff37186161569107
livius <livius@example.com>
2026-07-23 19:29:39 +0200

    Base-Pfad auf ai-security-score korrigiert

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ki-nutri-score/',
+  base: '/ai-security-score/',
   plugins: [react()],
 })
